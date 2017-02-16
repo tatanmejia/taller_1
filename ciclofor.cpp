492fff3ceb64b05fbdacb877cf8796ac66b91942
tatanmejia <jhotan_22m@hotmail.com>
2017-02-16 17:07:09 -0500

    Taller finalizado for

--- ciclofor.cpp
+++ ciclofor.cpp
@@ -19,7 +19,7 @@ int dato;
                 cout<<"2.Imprimir las tablas de multiplicar de un numero del 1 al 20: "<<endl;
                 cout<<"3. Solucionar el factorial de un numero: "<<endl;
                 cout<<"4. Solucionar el numero fibonacci: "<<endl;
-                cout<<"5. Solucionar el numero fibonacci: "<<endl;
+                cout<<"5. Ingrese 5 numeros aleatorios y almacenar en array, imprimirlos: "<<endl;
                 cout<<"0. SALIR "<<endl;
                 cout<<"Elija una opcion ";cin>>dato;
 
@@ -66,13 +66,11 @@ int dato;
                                 {
                             int n;
                             int t=1;
-                            int contad=1;
                             cout<<"ingrese un numero para calcular factorial"<<endl;
                             cin>>n;
-                          while(contad<=n)
+                         for(int i=1;i<=n;i++)
                                 {
-                                t= t* contad;
-                                contad++;
+                                    t=i*t;
                                 }
                             cout<<"El factorial de "<<n<<" es "<<t;
                             }
@@ -85,12 +83,47 @@ int dato;
 							system("cls");
 
 							cout<<"4. Solucionar el numero fibonacci: "<<endl;
+							int ant1, ant2,fib,numero;
+							ant1=1;
+							ant2=1;
+							fib=1;
+							cout<<"Ingrese numero de visualizacion de la  serie Fibonicce"<<endl;
+							cin>>numero;
+							cout<<ant1<<endl;
+							for(int i=2;i<=numero;i++){
+                                cout<<fib<<endl;
+                                fib=ant1+ant2;
+                                ant1=ant2;
+                                ant2=fib;
+							}
+                            getch();
+
+						break;
+
+                    case 5:
+
+							system("cls");
+
+							cout<<"5. Ingrese 5 numeros aleatorios y almacenar en array, imprimirlos: "<<endl;
+							int arreglo[5], i[5];
+							for(int i=0;i<5;i++){
+
+                                cout<<"Ingrese numeros para almacenar"<<endl;
+                                cin>>arreglo[i];
+							}
+							for(int i=0;i<5;i++){
+
+                                cout<<arreglo[i]<<endl;
+							}
+
 
 
 
                             getch();
 
 						break;
+
+
 					cout<<endl;
 					cout<<"El valor ingresado no es valido. Debe ser 0 , 1 , 2 , 3 , 4 , 5. "<<endl;
 				}
